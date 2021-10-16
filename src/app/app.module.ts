8644620a21299f5986da1bb6ba0aa773385f7ed8
othmaniahmed <mr.ahmedothmani@gmail.com>
2021-10-16 10:36:34 +0100

    ❤️First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { FirstPageComponent } from './home-component/first-page/first-page.component';
+import { NavBarComponent } from './home-component/nav-bar/nav-bar.component';
+import { SecondPageComponent } from './home-component/second-page/second-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponentComponent,
+    FirstPageComponent,
+    NavBarComponent,
+    SecondPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home-component/nav-bar/nav-bar.component.ts
@@ -0,0 +1,26 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  scroll_postion = false ;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @HostListener("document:scroll")
+  scrollfunction(){
+    if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 30){
+      this.scroll_postion = true;
+    }else{
+      this.scroll_postion = false ;
+    } 
+  }
+
+}
