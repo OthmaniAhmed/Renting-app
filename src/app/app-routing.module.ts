39975ad95e1b89c4ae9aef5e77a16678e61819cc
othmaniahmed <mr.ahmedothmani@gmail.com>
2021-10-22 14:06:33 +0100

    🤩 Adding Sign Up Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponentComponent } from './auth/login-component/login-component.component';
+import { SignupComponentComponent } from './auth/signup-component/signup-component.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponentComponent},
-  {path: 'login', component: LoginComponentComponent}
+  {path: 'login', component: LoginComponentComponent},
+  {path: 'sign', component:SignupComponentComponent}
 
 ];
 
