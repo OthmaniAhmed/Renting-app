99eb6462f3a16193a3470f4ed29b6005d7d5053f
othmaniahmed <mr.ahmedothmani@gmail.com>
2021-12-09 16:51:59 +0100

    🥶 Addig Filter Functionality

--- src/app/ads/ads-page/ads-page.component.ts
+++ src/app/ads/ads-page/ads-page.component.ts
@@ -9,31 +9,67 @@ import { AdsService } from '../ads.service';
 })
 export class AdsPageComponent implements OnInit {
 
+  prixByArray = ['All','Day','Week','Months','Year']
+  states = ['Any','Ariana', 'Béja', 'BenArous', 'Bizerte', 'Gabès', 'Gafsa', 'Jendouba', 'Kairouan', 'Kasserine', 'Kebili', 'Kef', 'Mahdia', 'Manouba', 'Medenine', 'Monastir', 'Nabeul', 'Sfax', 'Sidi Bouzid', 'Siliana', 'Sousse', 'Tataouine', 'Tozeur', 'Tunis','Zaghouan']
+
   ads : any = [] ;
   pageOfItmes : any = [];
   pageSize : number = 6 ;
 
   pageSlice = this.ads.slice(0 , 3)
 
+  filtredArray! : any [] ;
+  stateselectedOption ='' ;
+  prixByFilter = '' ;
+  numberOfRoomFilter: number | null = null ;
+
   constructor(private adsService : AdsService) { }
 
   ngOnInit(): void {
-    this.adsService.getAds().subscribe(data => this.ads = data);
+    this.adsService.getAds().subscribe(data => {
+      this.ads = data
+      this.pageSlice = this.ads.slice(0 , 3)
+      this.filtredArray = data;
+    });
 }
 
-pageClick(pageOfItmes : any){
-this.pageOfItmes = pageOfItmes;
-}
 
-OnPageChange(event : PageEvent){
-  
-  const startIndex = event.pageIndex * event.pageSize;
-  let endIndex = startIndex + event.pageSize;
-  if(endIndex > this.ads.length){
-    endIndex = this.ads.length ;
+filterFun(){
+  this.filtredArray = this.ads
+
+  if((this.stateselectedOption=='' || this.stateselectedOption == 'Any' ) && ( this.numberOfRoomFilter == null) && (this.prixByFilter =='All' || this.prixByFilter =='')){
+    this.filtredArray = this.ads
+  }else{
+    if( this.numberOfRoomFilter !=null){
+      this.filtredArray  = this.filtredArray.filter((ad: { roomNumber:  number; }) => ad.roomNumber == this.numberOfRoomFilter)
+    }
+    if(this.stateselectedOption != '' && this.stateselectedOption!='any')
+    this.filtredArray  = this.filtredArray.filter((ad: { ville: string; }) => ad.ville == this.stateselectedOption)
+    if(this.prixByFilter !='All' && this.prixByFilter !=''){
+      this.filtredArray  = this.filtredArray.filter((ad: { prixBy:  string; }) => ad.prixBy == this.prixByFilter )
+
+    }
+    
   }
-  this.pageSlice = this.ads.slice(startIndex, endIndex)
 
+
+  this.pageSlice = this.filtredArray.slice(0 , 3) ;
 }
+
+  pageClick(pageOfItmes : any){
+    this.pageOfItmes = pageOfItmes;
+  }
+  
+  OnPageChange(event : PageEvent){
+    
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if(endIndex > this.filtredArray.length){
+      endIndex = this.filtredArray.length ;
+    }
+    this.pageSlice = this.filtredArray.slice(startIndex, endIndex)
+  
+  }
+
 
 }
